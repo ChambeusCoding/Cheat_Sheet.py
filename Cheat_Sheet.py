49d2d868672b5e764d343c89359a75081607f2c2
ChambeusCoding <180331000+ChambeusCoding@users.noreply.github.com>
2025-03-06 22:00:05 -0500

    fefsf

---
+++ Cheat_Sheet.py
@@ -0,0 +1,37 @@
+#age = 18 ##age_variable
+#price = 19.95 ##price_variable
+#first_name = "Chambeus" ##name_variable
+#is_online = True ##boolean_variable
+
+#print (age) ##print_variable
+#print (price) ##print_variable
+#print (first_name) ##print_variable
+#print (is_online) ##print_variable
+
+###example47
+#first_name = "John"
+#last_name = "Smith"
+#age = 20
+#new_patient = True
+
+#name = input ("What is your name? ") ##Remember there is a space after the input in ()
+#variable inside of input (Supposedly important)
+#print ("Hello " + name)
+
+#VALUE CONVERSION (important)
+#birth_year = input ("Enter your birth year: ")
+#age = 2024 - int (birth_year)
+#print (age)
+#int () #(Built in functions)
+#float () #(Built in functions)
+#bool () #(Built in functions)
+#str () #(Built in functions)
+
+#First = input ("First: ") ##calculator
+#Second = input ("Second: ") ##calculator
+#sum = float (First) + float (Second) ##calculator
+#print (sum) #calculator
+
+##Function that Converts Str to UpperCase (SAME WITH .LOWER)
+#course = 'Python for Beginners'
+#print(course.upper())
